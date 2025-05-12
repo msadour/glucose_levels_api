6a533c56f9c3d97243413acf57791ecc41233bb5
Mehdi <sadour.mehdi@gmail.com>
2025-05-12 16:59:00 +0200

    Include user_id in url path.

--- source/apps/level/urls.py
+++ source/apps/level/urls.py
@@ -4,5 +4,5 @@ from .views import GlucoseLevelView
 
 urlpatterns = [
     path("", GlucoseLevelView.as_view()),
-    path("<str:user_id>/", GlucoseLevelView.as_view()),
+    path("user_id/<str:user_id>/", GlucoseLevelView.as_view()),
 ]
